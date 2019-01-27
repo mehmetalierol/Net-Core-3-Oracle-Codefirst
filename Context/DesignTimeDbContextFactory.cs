46acebc017d450a6dfbb875e42339c37771e6bb1
Mehmet Ali EROL <mehmetalierol@windowslive.com>
2019-01-27 21:31:42 +0300

    Initial Commit

---
+++ Context/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Net_Core_3_Oracle_Codefirst.Context
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<OracleDbContext>
+    {
+        public OracleDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<OracleDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseOracle(connectionString);
+            return new OracleDbContext(builder.Options);
+        }
+    }
+}
---
+++ Context/OracleDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Net_Core_3_Oracle_Codefirst.Entities;
+using System;
+
+namespace Net_Core_3_Oracle_Codefirst.Context
+{
+    public class OracleDbContext : DbContext
+    {
+        #region Constructor
+
+        public OracleDbContext(DbContextOptions<OracleDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        #endregion Constructor
+
+        #region ModelCreating
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        #endregion ModelCreating
+
+        #region OnConfiguring
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            optionsBuilder.UseOracle(config.GetConnectionString("DefaultConnection"));
+        }
+
+        #endregion OnConfiguring
+
+        #region DbSets
+
+        public DbSet<EntityUser> EntityUser { get; set; }
+
+        #endregion DbSets
+
+    }
+}
